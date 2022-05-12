ad4c12765ac82f1c938f2528ed0968062d3adf5f
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-05-12 15:23:37 -0700

    new 8kyu, modified and renamed old kata

--- js/find-stray-number.js
+++ js/find-stray-number.js
@@ -9,9 +9,7 @@
 
 function stray(numbers) {
     numbers.sort((a,b)=> a-b);
-    if(numbers[0]===numbers[1]){
-        return numbers[numbers.length-1]
-    } else {
-        return numbers[0]
-    }
+    if(numbers[0]===numbers[1]){return numbers[numbers.length-1]}
+    return numbers[0]
 }
+
---
+++ js/volume-of-cuboid.js
@@ -0,0 +1,12 @@
+// P: will the values always be valid? yes. will the values be ints or floats? both
+// R: return or console.log()? return
+// E: getVolumeOfCuboid(5, 6.6, 8) => 264
+// P: multiply arguments
+//    return answer
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return length*width*height
+    }
+}
+
