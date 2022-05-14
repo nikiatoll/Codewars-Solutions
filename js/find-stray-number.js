d139e43bd05083daa21ad49691ad96b859342cdf
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-05-14 11:00:42 -0700

    7kyu find stray number

---
+++ js/find-stray-number.js
@@ -0,0 +1,17 @@
+// P: numbers only? yes. only odd length? yes. need to check for invalid inputs? no
+// R: return or console.log? return
+// E: stray([1, 2, 1, 1, 1]) ==> 1
+//stray([5, 6, 5]) ==> 5
+// P: sort the numbers
+//    compare first and last with number in the middle (conditional)
+//    return non matching number
+
+
+function stray(numbers) {
+    numbers.sort((a,b)=> a-b);
+    if(numbers[0]===numbers[1]){
+        return numbers[numbers.length-1]
+    } else {
+        return numbers[0]
+    }
+}
