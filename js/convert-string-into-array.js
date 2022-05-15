fc9f83f094618a961859c5ee2305177d33172c03
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-05-15 09:26:37 -0700

    8kyu use .split()

---
+++ js/convert-string-into-array.js
@@ -0,0 +1,10 @@
+// Parameters: string only? yes. will the string always have a value? yes. punctuation? no
+// Returns: return or print? return. as an array of words or letters? words.
+// Examples: stringToArray("can we do it yes we can") = ["can", "we", "do", "it", "yes", "we", "can"]
+// PseudoCode:
+// .split(" ")
+//return 
+
+function stringToArray(string) {
+    return string.split(" ")
+}
