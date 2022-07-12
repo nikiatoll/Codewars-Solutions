75bce9258891e8116487b3f32fd0b1072932643b
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-07-12 19:03:54 -0400

    8kyu

---
+++ js/hello-world.js
@@ -0,0 +1,7 @@
+//P: params: takes no parameters
+//R: returns greeting "hellow world!"
+//E: greet() => "hello world!"
+//P: declare a function that will return the string "hello world!"
+
+// Write a function "greet" that returns "hello world!"
+const greet = () => "hello world!";
